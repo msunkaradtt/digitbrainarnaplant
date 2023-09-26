872cb3f40cc6e4d61a41f6fc95282b16e7b0aa63
msunkaradtt <mohith.sunkara@digitaltwin.technology>
2023-09-26 19:02:19 +0200

    changes 2 26-09-2023

--- digitbraindttapi/main.py
+++ digitbraindttapi/main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 from getdata import GetData
+from fastapi.encoders import jsonable_encoder
 
 app = fapi()
 getdata = GetData()
@@ -25,7 +26,15 @@ async def root(request: Request):
 async def updateSettings(request: Request):
     dp_data = await getdata.fetchEndpoint('http://dtpprocessingservice:3000/parameters')
     sol_data = await getdata.fetchEndpoint('http://solgeneratorservice:3001/parameters')
-    return templates.TemplateResponse("settings.html", {"request": request, "data_p": dp_data, "data_so": sol_data})
+    sch_data = await getdata.fetchEndpoint('http://secgeneratorservice:3002/parameters')
+    return templates.TemplateResponse("settings.html", {"request": request, "data_p": dp_data, "data_so": sol_data, "data_sc": sch_data})
+
+@app.post("/settings", include_in_schema=False)
+async def updateSettings(request: Request):
+    fmData = await request.form()
+    fmData = jsonable_encoder(fmData)
+    print(fmData)
+    return fmData
 
 @app.get("/updateall", response_class=HTMLResponse)
 async def updateall(request: Request):
